8d907e83f8a2c916da81dbc26dc6df754608cebe
gk-j <javvajigokul54@gmail.com>
2024-03-28 15:36:56 +0530

    changes_in_api

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -14,11 +14,11 @@ const RestaurantMenu = () =>{
    if(resInfo===null){
     return <MenuShimmer/>
    } 
-   
-    const { name, cuisines, costForTwoMessage } = resInfo?.cards?.[0]?.card?.card?.info 
-    const {itemCards}=  resInfo?.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card?.card
+    console.log(resInfo)
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards?.[2]?.card?.card?.info 
+    const {itemCards}=  resInfo?.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card?.card
     // console.log(resInfo?.cards[2].groupedCard.cardGroupMap.REGULAR.cards)
-    const categories= resInfo?.cards[2].groupedCard.cardGroupMap.REGULAR.cards.filter((c)=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+    const categories= resInfo?.cards[4].groupedCard.cardGroupMap.REGULAR.cards.filter((c)=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
     
     // console.log(itemCards)
     return(
